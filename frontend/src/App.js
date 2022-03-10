f336fe86bb5242136953f96a4adebb79b0113300
Patrick Cash <patrickcash@gmail.com>
2022-03-10 18:34:04 -0500

    basic routing and redux setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,36 @@
-function App() {
-  return (
-    <div className="App">
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Listings from './pages/Listings';
+import ListingDetail from './pages/ListingDetail';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import NotFound from './pages/NotFound';
+import Navbar from './components/Navbar';
 
-    </div>
+
+import { Provider } from 'react-redux';
+import store from './store';
+
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <Navbar />
+      <Router>
+        <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/about' component={About} />
+            <Route exact path='/contact' component={Contact} />
+            <Route exact path='/listings' component={Listings} />
+            <Route exact path='/listings/:id' component={ListingDetail} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/signup' component={SignUp} />
+            <Route component={NotFound} />
+        </Switch>
+      </Router>
+    </Provider>
   );
 }
 
---
+++ frontend/src/pages/ListingDetail.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ListingDetail() {
+  return (
+    <div>ListingDetail</div>
+  )
+}
